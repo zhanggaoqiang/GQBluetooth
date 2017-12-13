8b749cf498f531153b3ff3742bb030daa2ec1a31
张高强 <835389423@qq.com>
2017-12-13 14:09:04 +0800

    替换文件

--- GQBluetooth/BlueTooth.h
+++ GQBluetooth/BlueTooth.h
@@ -5,14 +5,22 @@
 //  Created by 张高强 on 2017/11/7.
 //  Copyright © 2017年 liuyanwei. All rights reserved.
 //
-
 #import <Foundation/Foundation.h>
-
 #import <CoreBluetooth/CoreBluetooth.h>
+/**
+ 枚举定义蓝牙所有指令状态
+ */
+typedef NS_ENUM(NSUInteger,CommandState){
+    CommandStateOpen = 0,//打开蓝牙
+    CommandStateClose,  //关闭蓝牙
+    CommandStateQuery   //查询蓝牙状态
+};
+
+
 @protocol BlueToothDelegate<NSObject>
 
 @optional
-//点击链接按钮
+//点击开始连接蓝牙按钮
 -(void)didClickStart;
 //连接成功
 - (void)BLEConnectSucceed;
@@ -20,21 +28,10 @@
 - (void)receivedValue:(NSString *)data;
 //打开蓝牙提示
 -(void)openBluetooth;
-
 //连接蓝牙失败
 -(void)connectFail;
 
--(void)notFindDevice;
-
-
-
-
 @end
-
-
-
-
-
 @interface BlueTooth : NSObject<CBPeripheralDelegate,CBCentralManagerDelegate>
 
 //代理
@@ -45,25 +42,16 @@
 @property (nonatomic,strong) CBPeripheral *peripheral;
 //特征
 @property (nonatomic, strong) CBCharacteristic *characteristic;
-@property(nonatomic,copy)NSString *chaxun;
+@property(nonatomic,strong)CBCentralManager *central;//中心设备管理
+@property(nonatomic,copy)NSString *peripheralName;//传入的需要连接的外设
+@property(nonatomic,assign)CommandState command;//外设具体指令状态
 
-@property(nonatomic,strong)NSMutableArray *allDeviceList;
-@property(nonatomic,strong)CBCentralManager *central;
-@property(nonatomic,copy)NSString *zih;
+
 //初始化
 + (instancetype)sharedInstance;
 //开始连接
 - (void)startConnect;
 //断开连接
 - (void)endConnect;
-//读数据
-- (void)readFromPeripheral;
-//写数据
-- (void)writeToPeripheralWith:(NSString *)name;
-//监听数据
-- (void)notifyPeripheral;
-
-
-
 
 @end
--- GQBluetooth/BlueTooth.m
+++ GQBluetooth/BlueTooth.m
@@ -7,68 +7,63 @@
 //
 
 #import "BlueTooth.h"
-
-
-
 @implementation BlueTooth
-
+ static id _instance;
+/**
+ 创建单例对象
+ @return 返回唯一实例
+ */
 +(instancetype)sharedInstance{
-    BlueTooth *manager=nil;
-    //    static dispatch_once_t onceToken;
-    //    dispatch_once(&onceToken, ^{
-    manager=[[BlueTooth alloc] init];
-    manager.chaxun=@"";
-
-   
-    
-    
-    
-    //    });
-    return manager;
+    @synchronized(self){
+        if(_instance == nil){
+            _instance = [[self alloc] init];
+        }
+    }
+    return _instance;
 }
-
-
+/**
+ 开始连接蓝牙
+ */
 -(void)startConnect{
-    _allDeviceList=[NSMutableArray arrayWithCapacity:100];
-    
     if (self.delegate && [self.delegate respondsToSelector:@selector(didClickStart)])
     {
         //控制器更新UI
         [self.delegate didClickStart];
     }
-    
     //初始化中心端,开始蓝牙模块
     self.centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil options:nil];
     self.centralManager.delegate = self;
-   
 }
 
 //断开连接
 - (void)endConnect
 {
     [self.centralManager cancelPeripheralConnection:self.peripheral];
+       _centralManager=nil;
 }
-
 #pragma mark - CBCentralManagerDelegate
 // 状态更新后触发
 -(void)centralManagerDidUpdateState:(CBCentralManager *)central
 {
     switch (central.state) {
-        case CBCentralManagerStatePoweredOff:{
+        case CBManagerStatePoweredOff:{
             NSLog(@"蓝牙关闭");
-            [self openLanya];
-            
+            //检测到蓝牙没打开需要通知代理控制器去执行相关提示操作
+            if (self.delegate && [self.delegate respondsToSelector:@selector(openBluetooth)])
+            {
+                [self.delegate openBluetooth];
+            };
         }
             break;
-        case CBCentralManagerStatePoweredOn:
+        case CBManagerStatePoweredOn:
             break;
-        case CBCentralManagerStateResetting:
+        case CBManagerStateResetting:
             break;
-        case CBCentralManagerStateUnauthorized:
+        case CBManagerStateUnauthorized:
             break;
-        case CBCentralManagerStateUnknown:
+        case CBManagerStateUnknown:
             break;
-        case CBCentralManagerStateUnsupported:
+        case CBManagerStateUnsupported:
             break;
         default:
             break;
@@ -76,66 +71,32 @@
     [central scanForPeripheralsWithServices:nil options:nil];
 }
 
-
--(void)openLanya{
-    if (self.delegate && [self.delegate respondsToSelector:@selector(openBluetooth)])
-    {
-        //控制器更新UI
-        [self.delegate openBluetooth];
-    }
-    
-}
-
-- (void)delayMethod{
-//    NSLog(@"delayMethodEnd");
-//
-//    _centralManager=nil;
-//    [self.timer invalidate];
-//
-//    BOOL isbool=[_allDeviceList containsObject:@"ZIH0491300"];
-//    if (!isbool) {
-//        if ([self.delegate respondsToSelector:@selector(notFindDevice)]) {
-//            [self.delegate notFindDevice];
-//        }
-//    }
-
-}
-
-
-
 // 扫描到外部设备后触发的代理方法//多次调用的
 - (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(nonnull CBPeripheral *)peripheral advertisementData:(nonnull NSDictionary<NSString *,id> *)advertisementData RSSI:(nonnull NSNumber *)RSSI
 {
-    [_allDeviceList addObject:peripheral];
-    
     NSString *msg = [NSString stringWithFormat:@"信号强度: %@, 外设: %@", RSSI, peripheral];
     NSLog(@"%@",msg);
     //ZIH0491300//
-    
-    if ([self.chaxun isEqualToString:@"chaxun"]){
-        NSString *scanStr=[[NSUserDefaults standardUserDefaults] objectForKey:@"sevNumber"];
-        NSString *lastFiveChartect=[scanStr substringFromIndex:scanStr.length-7];
-        self.zih=[@"ZIH" stringByAppendingString:lastFiveChartect];
-    }else{
-    NSString *scanStr=[[NSUserDefaults standardUserDefaults] objectForKey:@"scanStr"];
-    NSString *lastFiveChartect=[scanStr substringFromIndex:scanStr.length-7];
-    self.zih=[@"ZIH" stringByAppendingString:lastFiveChartect];
-    }
-//    NSString *name=[@"ZIH" stringByAppendingString:lastFiveChartect];
-    if ([peripheral.name isEqualToString:self.zih])
-    {
-        //连接外部设备
-        self.peripheral = peripheral;
-        [central connectPeripheral:peripheral options:nil];
-        //停止搜索
-        self.central=central;
-        [central stopScan];
-   
-    }else {
-//        if ([self.delegate respondsToSelector:@selector(notFindDevice)]) {
-//            [self.delegate notFindDevice];
-//        }
-    }
+    NSLog(@"此时外设名字是%@",_peripheralName);
+    NSAssert(![_peripheralName isEqualToString:@""]||_peripheralName==nil, @"please init correct peripheralName");
+        if ([peripheral.name isEqualToString:_peripheralName])
+        {
+            //连接外部设备
+            self.peripheral = peripheral;
+            [central connectPeripheral:peripheral options:nil];
+            //停止搜索
+            self.central=central;
+            [central stopScan];
+            
+        }else{
+            
+        }
+}
+
+//处理Objective-C的断言
+- (void)handleFailureInMethod:(SEL)selector object:(id)object file:(NSString *)fileName lineNumber:(NSInteger)line description:(NSString *)format,...
+{
+    NSLog(@"NSAssert Failure: Method %@ for object %@ in %@#%li", NSStringFromSelector(selector), object, fileName, (long)line);
 }
 
 //连接失败
@@ -147,35 +108,24 @@
     }
 }
 
-
 // 当中心端连接上外设时触发
 - (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral
 {
     NSLog(@"连接上外设");
-    [[NSUserDefaults standardUserDefaults] setObject:@"1" forKey:@"connect"];
     [central stopScan];
     if ([self.delegate respondsToSelector:@selector(BLEConnectSucceed)]) {
         [self.delegate BLEConnectSucceed];
     }
-    
     self.peripheral.delegate = self;
-    
     [peripheral discoverServices:nil];
-    
-    
 }
 
 
 //如果连接上的两个设备突然断开了，程序里面会自动回调下面的方法
 -   (void)centralManager:(CBCentralManager *)central didDisconnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error
 {
-    NSLog(@"设备断开重连");
-    [self.centralManager connectPeripheral:self.peripheral options:nil];
-    //    self.centralManager=nil;
-    //当断开时做缺省数据处理
+    NSLog(@"已经断开蓝牙连接");
 }
-
-
 #pragma mark - CBPeripheralDelegate
 // 外设端发现了服务时触发
 - (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error
@@ -188,11 +138,7 @@
     }
     for (CBService *service in peripheral.services)
     {
-        //只找有用的服务
-        //        if ([service.UUID.description isEqualToString:@"服务UUID名称"])
-        //        {
         [peripheral discoverCharacteristics:nil forService:service];
-        //        }
     }
 }
 
@@ -203,40 +149,20 @@
     NSLog(@"%@",service.characteristics);
     for (CBCharacteristic *characteristic in service.characteristics)
     {
-        // -------- 读特征的处理 --------
-        //        if ([characteristic.UUID.description isEqualToString: @"读特征名称"])
-        //        {
-        //            NSLog(@"处理读特征");
-        //            [self.peripheral readValueForCharacteristic:characteristic];
-        //        }
-        //
-        //        // -------- 写特征的处理 --------
-        //        if ([characteristic.UUID.description isEqualToString: @"0003CDD2-0000-1000-8000-00805F9B0131"])
-        //        {
-        //            NSLog(@"处理写特征");
-        //            //向外设发送0001命令
-        if ([self.chaxun isEqualToString:@"chaxun"]) {
+        if (self.command==CommandStateQuery) {
             NSData *data = [@"" dataUsingEncoding:NSUTF8StringEncoding];
             [self.peripheral writeValue:data forCharacteristic:characteristic type:0x04];
             self.characteristic = characteristic;
-        }else{
+        }
+        
+        if (self.command==CommandStateOpen) {
             NSData *data = [@"&123&123&1&" dataUsingEncoding:NSUTF8StringEncoding];
             [self.peripheral writeValue:data forCharacteristic:characteristic type:0x04];
             self.characteristic = characteristic;
-            
         }
-       
-        //        }
-        //
-        // -------- 订阅特征的处理 --------
-        //        if ([characteristic.UUID.description isEqualToString: @"订阅特征名称"])
-        //        {
-        NSLog(@"处理了订阅特征");
         [peripheral setNotifyValue:YES forCharacteristic:characteristic];
-        //        }
     }
 }
-
 
 //将十六进制的字符串转换成NSString则可使用如下方式:
 + (NSString *)convertHexStrToString:(NSString *)str {
@@ -268,70 +194,14 @@
 }
 
 
-
-//收到数据
+//收到数据,并且通知代理接受数据，并实现相关功能
 - (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
-
 {
-    NSLog(@"收到的数据是%@",characteristic.value);
     NSString *results=[[NSString alloc] initWithData:characteristic.value encoding:NSUTF8StringEncoding];
-    NSLog(@"收到的数据是%@",results);
         if ([self.delegate respondsToSelector:@selector(receivedValue:)]) {
             [self.delegate receivedValue:results];
-            
         }
-    _centralManager=nil;
-
 }
-
-
-// 写特征CBCharacteristicWriteWithResponse的数据写入的结果回调
-- (void)peripheral:(CBPeripheral *)peripheral didWriteValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
-{
-    if (error) {
-        NSLog(@"数据写入失败: %@", error);
-        
-    } else {
-        NSLog(@"数据写入成功");
-        [peripheral readValueForCharacteristic:characteristic];
-    }
-}
-
-#pragma mark - VCMethod
-//读数据
-- (void)readFromPeripheral
-{
-    NSLog(@"读数据");
-    [self.peripheral readValueForCharacteristic:self.characteristic];
-}
-
-//"&123&123&1&"
-//写数据
-- (void)writeToPeripheralWith:(NSString *)instruct
-{
-    //    NSLog(@"写数据");
-    //
-    //    NSData *data = [@"2631323326313233263126" dataUsingEncoding:NSUTF8StringEncoding];
-    //    [self.peripheral writeValue:data forCharacteristic:self.characteristic type:CBCharacteristicWriteWithoutResponse];
-    ////    NSData *data = [[BlueTooth sharedTool] hexToBytes:instruct];
-    ////    [self.peripheral writeValue:data forCharacteristic:self.characteristic type:CBCharacteristicWriteWithoutResponse];
-}
-
-//监听数据
-- (void)notifyPeripheral
-{
-    NSLog(@"监听数据");
-    [self.peripheral setNotifyValue:YES forCharacteristic:self.characteristic];
-    //    NSString *dataFormat=@"";
-    //    if ([dataFormat isEqualToString:@"返回数据格式有误"]) {
-    //        NSLog(@"返回格式错误");
-    //
-    //    }
-}
-
-
-
-
 
 @end
 
